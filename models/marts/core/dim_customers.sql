b5354f6fc9a7dc3b9cd20430726c420789e5f33f
rinorsefa@hotmail.com <rinorsefa@hotmail.com>
2023-06-18 19:12:20 +0000

    updates query sources

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,9 +1,5 @@
-with customers as (
-    select * from {{ ref ('stg_jaffle_shop_customers')}}
-)
-
 select
-    customers.customer_id,
-    customers.first_name,
-    customers.last_name
-from customers
+    customer_id,
+    first_name,
+    last_name
+from from {{ source('jaffle_shop', 'customers') }}
